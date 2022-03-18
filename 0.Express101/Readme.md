90dcfea5a20a4fbb158a41a9aca903de3f8781ee
Mr-Anik1 <anik2002bro@gmail.com>
2022-03-18 15:55:23 -0700

    Express

---
+++ 0.Express101/Readme.md
@@ -0,0 +1,34 @@
+nodemon install করার কারণ হল, বার বার আমাদের server on/off করতে
+হবে না। code save করলেই automatic server reload হবে।
+
+**কোন কিছু "devDependencies" আকারে install করার জন্য**
+**npm i -D [package name]**
+-D দ্বারা "devDependencies" বুঝায় 
+
+ 
+
+{
+  "name": "0.express101",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {**এই script গুলা নিজের সুবিধার জন্য লিখা, npm run dev/start দিলে প্রোজেক্ট run করবে**
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  }, 
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "express": "^4.17.3"
+  },
+  "engines": {**Node এর কোন version ব্যবহার করছি সেটা, ফলে অন্য ডেভেলপার সহজেই বুঝতে পারবে**
+    "node": "16.14.0"
+  },
+  "devDependencies": {**এটা project এর কোন অংশ না,এটা Developer এর সুবিধার জন্য**
+    "nodemon": "^2.0.15"
+  }
+}
+
+
+
---
+++ 0.Express101/index.js
@@ -0,0 +1,101 @@
+const express=require("express");//Express Import 
+
+const fs=require('fs');
+
+const app=express();
+
+
+/* 
+  '/' দ্বারা root route বোঝায়, user যখন root route এ hit করবে 
+  ডাটা দেখার জন্য,তখন তাকে response back দিতে হবে। যেটা res.send() 
+  এর মাধ্যমে করা হয়েছে। 
+*/
+app.get('/',(req,res)=>{
+    res.send(//user কে একটা html file send করলাম 
+    `
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Node JS</title>
+    
+        <style>
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+    
+            body {
+                font-family: Arial, Helvetica, sans-serif;
+                font-size: 16px;
+            }
+    
+            .container {
+                width: 50%;
+                margin: 2rem auto;
+                padding: 2rem;
+                background: #dddeee;
+            }
+            h1{
+                color:rgb(6, 129, 680);
+            }
+        </style>
+    
+    </head>
+    
+    <body>
+      <div class="container">
+        <h1>Nodemon Save Our Time</h1></br>
+         <p>
+          Anik Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ab dolor, tempore error delectus, dolore
+          odio rem minus labore qui dicta aliquam, quidem facere? Dignissimos assumenda quisquam vero iusto soluta!
+         </p>
+     </div>
+    </body>
+    
+    </html>
+
+    `
+    );
+});
+
+
+/*
+  html file টা fs require এর মাধ্যমে send করলাম, ফলে এখন আর 
+  code এর মধ্যে `` ব্যবহার করে html code লিক্তহে হবে না। 
+*/
+app.get('/about',(req,res)=>{
+  fs.readFile('pages/about.html',(err,data)=>{//pages folder এর about file import করলাম 
+      if(err){
+          console.log("Error",err);
+          res.send(`<h1>Something is wrong,check error message</h1>`);
+      }else{
+          res.write(data);
+          res.end();
+      }
+  });
+});
+  
+
+//help route
+app.get('/help',(req,res)=>{
+    fs.readFile('pages/help.html',(err,data)=>{
+        if(err){
+            console.log("Error",err);
+            res.send(`<h1>Something is wrong</h1>`);
+        }else{
+            res.write(data);
+            res.end();
+        }
+    })
+});
+
+
+
+app.listen(4000,()=>{//কোন port এ এটা run হবে 
+    console.log("Server is listening on port 4000");
+});
